7ee9068512fb29366a40e766db4a8f391315f2b7
muhammadeducation06-png <muhammad.education06@gmail.com>
2026-03-04 09:59:21 +0500

    react

---
+++ src/Components/ClickEvents.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ClickEvents = () => {
+
+    let firtName = 'Fuzail';
+
+    const handleClick = () => {
+        let firtName = 'Abdullah'; 
+        alert(`Hey! ${firtName}`);
+    }
+
+    console.log('Rendering')
+
+  return (
+    <div>
+        <h2>Hey, {firtName}</h2>
+        <button onClick={handleClick}>Click Me</button> <br />
+        <button onClick={() => alert(`Hey! ${firtName}`)}>Click Me</button>
+
+    </div>
+  )
+}
+
+export default ClickEvents
---
+++ src/Components/JSX.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const JSX = () => {
+  return (
+    <div>
+        <h2>
+            Java-Scriptand XML (20 + 30) = {20+30}
+        </h2>
+    </div>
+  )
+
+// React WithOut JSX
+
+// return React.createElement("div", null, React.createElement("h2", null, "Java-Scriptand XML (20 + 30) = ", 20 +30 ))
+}
+
+export default JSX
